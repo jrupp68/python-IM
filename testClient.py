393cf284226faeb8baddddc32e3de7a4ba000535
Jeff Rupp <ruppjj29@uww.edu>
2016-04-18 20:03:08 -0500

    Testing different protocol formats

--- testClient.py
+++ testClient.py
@@ -1,22 +1,22 @@
 from tkinter import*
 from socket import*
 
-root = Tk()
-root.title("IM Service")
-root.minsize(width=300,height=300)
+# root = Tk()
+# root.title("IM Service")
+# root.minsize(width=300,height=300)
 
-im = Message(root, text="Test Message", width=200)
-im.grid(row=0, column=1)
-im.pack
+# im = Message(root, text="Test Message", width=200)
+# im.grid(row=0, column=1)
+# im.pack
 
-msgLabel = Label(root, text="Message :")
-msgLabel.grid(row = 5, column = 0)
+# msgLabel = Label(root, text="Message :")
+# msgLabel.grid(row = 5, column = 0)
 
-msg = Entry(root)
-msg.grid(row = 5, column = 1)
+# msg = Entry(root)
+# msg.grid(row = 5, column = 1)
 
-send = Button(root, text="Send")
-send.grid(row = 5, column = 2)
+# send = Button(root, text="Send")
+# send.grid(row = 5, column = 2)
 #friend1 = Button(root,text="Jeff")
 #friend1.grid(row=0)
 #friend2 = Button(root,text="Adam")
@@ -24,4 +24,35 @@ send.grid(row = 5, column = 2)
 #friend3 = Button(root,text="John")
 #friend3.grid(row=2)
 
-root.mainloop()
+# root.mainloop()
+
+serverName = "192.168.0.102"
+serverPort = 12009
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+
+def getFriends():
+	outgoing = "GET_FRIENDS \t JRUPP68"
+	clientSocket.send(outgoing.encode())
+
+	incoming = clientSocket.recv(1024)
+	print(incoming.decode('ascii'))
+
+def getOfflineMessages():
+	outgoing = "GET_OFFLINE_MESSAGES \t JRUPP68"
+	clientSocket.send(outgoing.encode())
+
+	incoming = clientSocket.recv(1024)
+	print(incoming.decode('ascii'))
+
+def getNewMessages():
+	outgoing = "GET_NEW_MESSAGES \t JRUPP68"
+	clientSocket.send(outgoing.encode())
+
+	incoming = clientSocket.recv(1024)
+	print(incoming.decode('ascii'))
+
+	
+getFriends()
+
+
