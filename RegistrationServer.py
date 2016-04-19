10e3075109ebaac1195b8b1dcb9fb6c33617b32d
jrupp68 <ruppjj29@uww.edu>
2016-04-19 17:00:59 -0500

    added a lot of functionality
    registration, view friend list, send message, view messages, login, and
    adding friends all functional

--- RegistrationServer.py
+++ RegistrationServer.py
@@ -11,145 +11,203 @@ print("Ready")
 while 1:
     connectionSocket, address = serverSocket.accept()
 
-    #Wait for incoming message
-    incoming1 = connectionSocket.recv(1024).decode('ascii')
+    while 1:
+        #Wait for incoming message
+        incoming1 = connectionSocket.recv(1024).decode('ascii')
+        print(incoming1)
+        #Get request out of incoming message
+        request = incoming1.split("\t")[0].strip()
 
-    #Get request out of incoming message
-    request = incoming1.split("\t")[0].strip()
+        outgoing = ""
 
-    outgoing = ""
+        #Check request and perform action
+        if(request == "REGISTER"):
+            
+            register = incoming1+"\n"
 
-    #Check request and perform action
-    if(request == "REGISTER"):
-        
-        register = incoming1+"\n"
-
-        #split the the data received into variables
-        userName= register.split("\t")[1].strip()
-        password= register.split("\t")[2].strip()
-        passwordCheck= register.split("\t")[3].strip()
-        firstName= register.split("\t")[4].strip()
-        lastName= register.split("\t")[5].strip()
-        email= register.split("\t")[6].strip()
-        registerStatus = "GOOD"
-        
-        #Check if the length of password not <6
-        if(len(password)<6):
-            registerStatus="SHORT"
-            registerMessage= "Registration Status: Password Too Short"
-            print(registerMessage)
-            # Don't send message to Client here
-            #connectionSocket.send(registerStatus.encode())
-
-        elif(password != passwordCheck):
-            registerStatus="RETYPE"
-            registerMessage= "Registration Status: Passwords Do Not Match. Re-Enter Passwords"
-            print(registerMessage)
-            #connectionSocket.send(registerStatus.encode())
-
-        else:
-            for line in open("RegisterFile.txt",'r'):
-                if(line != ""):
-                    print(line)
-                    registeredID= line.split("\t")[0].strip()
-                    regFirstName= line.split("\t")[2].strip()
-                    regLastName= line.split("\t")[3].strip()
-
-                    #Check if the userName/userID is already registered.
-                    if(userName == registeredID):
-                        #If registered check with First Name and Last
-                        if(regFirstName == firstName and regLastName == lastName):
-                            registerStatus = "EXIST"
-                            registerMessage= "Register Status: You Are Already Registered"
-                            print(registerMessage)
-                            #connectionSocket.send(registerStatus.encode())
-                        else:
-                            registerStatus= "TAKEN"
-                            registerMessage= "Register Status: User Name Unavailable"
-                            print(registerMessage)
-                            #connectionSocket.send(registerStatus.encode())
-            #If everything is correct, Register
-            if(registerStatus == "GOOD"):
-                forRegister = userName+"\t"+password+"\t"+firstName+"\t"+lastName+"\t"+email+"\n"
-                output_file= open("RegisterFile.txt", 'a')
-                output_file.write(forRegister)
-                output_file.close()
-                filename = userName+"_FRIENDS.txt"
-                output_file = open(filename, 'a')
-                output_file.close()
-                registerStatus= "SUCCESS"
-                registerMessage= "Register Status: Registration Was Successful"
+            #split the the data received into variables
+            userName= register.split("\t")[1].strip().upper()
+            password= register.split("\t")[2].strip().upper()
+            passwordCheck= register.split("\t")[3].strip().upper()
+            firstName= register.split("\t")[4].strip().upper()
+            lastName= register.split("\t")[5].strip().upper()
+            email= register.split("\t")[6].strip().upper()
+            registerStatus = "GOOD"
+            
+            #Check if the length of password not <6
+            if(len(password)<6):
+                registerStatus="SHORT"
+                registerMessage= "Registration Status: Password Too Short"
+                print(registerMessage)
+                # Don't send message to Client here
                 #connectionSocket.send(registerStatus.encode())
 
-        connectionSocket.send(registerStatus.encode())
+            elif(password != passwordCheck):
+                registerStatus="RETYPE"
+                registerMessage= "Registration Status: Passwords Do Not Match. Re-Enter Passwords"
+                print(registerMessage)
+                #connectionSocket.send(registerStatus.encode())
 
-    elif(request == "GET_FRIENDS"):
-        user = incoming1.split("\t")[1].strip()
-        filename = user+"_FRIENDS.txt"
-        file = open(filename, 'r')
-        for line in file:
-            outgoing += line.split("\t")[0].strip() + "\t\t" + line.split("\t")[1].strip() + "\n"
-        connectionSocket.send(outgoing.encode())
-        file.close()
-
-    elif(request == "GET_OFFLINE_MESSAGES"):
-        user = incoming1.split("\t")[1].strip()
-        #Add code for offline messages
-        
-    elif(request == "GET_NEW_MESSAGES"):
-        user = incoming1.split("\t")[1].strip()
-        friend = incoming1.split("\t")[2].strip()
-        #Add code for getting new messages
-
-    elif(request == "SEND_MESSAGE"):
-        user = incoming1.split("\t")[1].strip()
-        friend = incoming1.split("\t")[3]
-        message = incoming1.split("\t")[2]
-
-    elif(request == "ADD_FRIEND"):
-        username = incoming1.split("\t")[1].strip()
-        friend = incoming1.split("\t")[2].strip()
-
-        file = open("RegisterFile.txt", 'r')
-        for line in file:
-            friendCheck = line.split("\t")[0].strip()
-            if(friend == friendCheck):
-                filename = username + "_FRIENDS.txt"
-                writeFile = open(filename, 'a')
-                writeFile.write(friend)
-                print(friend)
-                writeFile.close
-                outgoing = "SUCCESS"
-
-        file.close()
-        connectionSocket.send(outgoing.encode())
-
-
-        # respond success and new friends list with status
-
-    elif(request == "LOGIN"):
-        username = incoming1.split("\t")[1].strip()
-        print(username)
-        password = incoming1.split("\t")[2].strip()
-        print(password)
-        file = open("RegisterFile.txt", 'r')
-        for line in file:
-            usernameCheck = line.split("\t")[0].strip()
-            print(usernameCheck)
-            if(username == usernameCheck):
-                passwordCheck = line.split("\t")[1].strip()
-                print(password)
-                if(password == passwordCheck):
-                    outgoing = "SUCCESS"
-                else:
-                    outgoing = "WRONG_PASSWORD"
             else:
-                outgoing = "BAD_USERNAME"
-        file.close()
-        connectionSocket.send(outgoing.encode())
-        
-    else:
-        print("Invalid Request")
+                for line in open("RegisterFile.txt",'r'):
+                    print(line)
+                    if(line != ""):
+                        print(line)
+                        registeredID= line.split("\t")[0].strip().upper()
+                        regFirstName= line.split("\t")[2].strip().upper()
+                        regLastName= line.split("\t")[3].strip().upper()
+
+                        #Check if the userName/userID is already registered.
+                        if(userName == registeredID):
+                            #If registered check with First Name and Last
+                            if(regFirstName == firstName and regLastName == lastName):
+                                registerStatus = "EXIST"
+                                registerMessage= "Register Status: You Are Already Registered"
+                                print(registerMessage)
+                                #connectionSocket.send(registerStatus.encode())
+                            else:
+                                registerStatus= "TAKEN"
+                                registerMessage= "Register Status: User Name Unavailable"
+                                print(registerMessage)
+                                #connectionSocket.send(registerStatus.encode())
+                #If everything is correct, Register
+                if(registerStatus == "GOOD"):
+                    forRegister = userName+"\t"+password+"\t"+firstName+"\t"+lastName+"\t"+email+"\n"
+                    output_file= open("RegisterFile.txt", 'a')
+                    output_file.write(forRegister)
+                    output_file.close()
+                    filename = userName+"_FRIENDS.txt"
+                    output_file = open(filename, 'a')
+                    output_file.close()
+                    output_file = open("ONLINE_STATUS.txt", 'a')
+                    toWrite = userName + "\t" + "OFFLINE" + "\n"
+                    output_file.write(toWrite);
+                    output_file.close()
+                    filename = userName+"_MESSAGES.txt"
+                    output_file = open(filename, 'a')
+                    output_file.close()
+                    registerStatus= "SUCCESS"
+                    registerMessage= "Register Status: Registration Was Successful"
+                    #connectionSocket.send(registerStatus.encode())
+
+            connectionSocket.send(registerStatus.encode())
+
+        elif(request == "GET_FRIENDS"):
+            user = incoming1.split("\t")[1].strip().upper()
+            filename = user+"_FRIENDS.txt"
+            file = open(filename, 'r')
+            for line in file:
+                outgoing += line.split("\t")[0].strip().upper()
+                statusFile = open("ONLINE_STATUS.txt", 'r')
+                for line2 in statusFile:
+                    if(line2.split("\t")[0].strip().upper() == line.split("\t")[0].strip().upper()):
+                        outgoing += "\t\t" + line2.split("\t")[1].strip().upper() + "\n"
+                        break
+            connectionSocket.send(outgoing.encode())
+            file.close()
+
+        elif(request == "GET_OFFLINE_MESSAGES"):
+            user = incoming1.split("\t")[1].strip().upper()
+            #Add code for offline messages
+            
+        elif(request == "GET_NEW_MESSAGES"):
+            user = incoming1.split("\t")[1].strip().upper()
+            friend = incoming1.split("\t")[2].strip().upper()
+            #Add code for getting new messages
+
+        elif(request == "SEND_MESSAGE"):
+            user = incoming1.split("\t")[1].strip().upper()
+            friend = incoming1.split("\t")[3].strip().upper()
+            message = incoming1.split("\t")[2].strip()
+
+            filename = friend + "_MESSAGES.txt"
+            file = open(filename, 'a')
+            writeMessage = message + "\t" + user + "\n"
+            a = file.write(writeMessage)
+            print(a)
+            file.close()
+
+            if(a > 1):
+                outgoing = "SUCCESS"
+            else:
+                outgoing = "FAILED"
+
+            connectionSocket.send(outgoing.encode())
+
+        elif(request == "ADD_FRIEND"):
+            username = incoming1.split("\t")[1].strip().upper()
+            friend = incoming1.split("\t")[2].strip().upper()
+
+            file = open("RegisterFile.txt", 'r')
+            for line in file:
+                friendCheck = line.split("\t")[0].strip().upper()
+                if(friend == friendCheck):
+                    file.close()
+                    filename = username + "_FRIENDS.txt"
+                    print(filename)
+                    writeFile = open(filename, 'a')
+                    a = writeFile.write(friend+'\n')
+                    print(a)
+                    print(friend)
+                    writeFile.close()
+                    outgoing = "SUCCESS"
+                    break
+                else:
+                    outgoing = "FAILED"
+
+            file.close()
+            connectionSocket.send(outgoing.encode())
+
+
+            # respond success and new friends list with status
+
+        elif(request == "LOGIN"):
+            username = incoming1.split("\t")[1].strip().upper()
+
+            password = incoming1.split("\t")[2].strip().upper()
+
+            file = open("RegisterFile.txt", 'r')
+            for line in file:
+                usernameCheck = line.split("\t")[0].strip().upper()
+
+                if(username == usernameCheck):
+                    passwordCheck = line.split("\t")[1].strip().upper()
+
+                    if(password == passwordCheck):
+                        outgoing = "SUCCESS"
+                        file = open("ONLINE_STATUS.txt", 'r')
+                        filedata = file.read()
+                        file.close
+
+                        newdata = filedata.replace(username + "\t" + "OFFLINE", username + "\t" + "ONLINE")
+                        file = open("ONLINE_STATUS.txt", 'w')
+                        file.write(newdata)
+                        file.close()
+                        break            
+                    else:
+                        outgoing = "WRONG_PASSWORD"
+                else:
+                    outgoing = "BAD_USERNAME"
+            file.close()
+            connectionSocket.send(outgoing.encode())
+
+        elif(request == "VIEW_ALL_MESSAGES"):
+            user = incoming1.split("\t")[1].strip().upper()
+
+            filename = user + "_MESSAGES.txt"
+            file = open(filename, 'r')
+            outgoing = ""
+            for line in file:
+                outgoing += line.split("\t")[1].strip().upper() + ": " + line.split("\t")[0].strip()
+            file.close()
+            connectionSocket.send(outgoing.encode())
+
+        elif(request == "LOGOUT"):
+            #add online_status chagne to offline here
+            user = incoming1.split("\t")[1].strip().upper()
+            connectionSocket.close()
+        else:
+            print("Invalid Request")
 
 connectionSocket.close()
         
--- testClient.py
+++ testClient.py
@@ -31,14 +31,7 @@ serverPort = 12009
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-def register():
-    username = input("Username :")
-    password = input("Password :")
-    passwordCheck = input("Password :")
-    firstName = input("First Name :")
-    lastName = input("Last Name :")
-    email = input("Email :")
-
+def register(username, password, passwordCheck, firstName, lastName, email):
     outgoing = "REGISTER\t" + username + "\t" + password + "\t" + passwordCheck + "\t" + firstName + "\t" + lastName + "\t" + email
     clientSocket.send(outgoing.encode())
 
@@ -48,10 +41,7 @@ def register():
     #If register is success then go to login page
     #If register fails then prompt user to re register
 
-def login():
-    username = input("Username :")
-    password = input("Password :")
-
+def login(username, password):
     outgoing = "LOGIN\t" + username + "\t" + password
     clientSocket.send(outgoing.encode())
 
@@ -61,17 +51,16 @@ def login():
     #If login is success then open window with user friends list
     #If login fails, display why and prompt user to try again
     
-def getFriends():
-    outgoing = "GET_FRIENDS\tJRUPP68"
+def getFriends(username):
+    outgoing = "GET_FRIENDS\t" + user
     clientSocket.send(outgoing.encode())
 
     incoming = clientSocket.recv(1024)
     print("Friends \t Status")
     print(incoming.decode('ascii'))
 
-def addFriend():
-    friend = input("Please enter the username of the friend you would like to add :")
-    outgoing = "ADD_FRIEND\tjeff\t" + friend
+def addFriend(user, friend):
+    outgoing = "ADD_FRIEND\t" + user + "\t" + friend
 
     clientSocket.send(outgoing.encode())
 
@@ -79,50 +68,86 @@ def addFriend():
     print(incoming.decode('ascii'))
         
 def getOfflineMessages():
-    outgoing = "GET_OFFLINE_MESSAGES \t JRUPP68"
+    user = input("Please enter your username :")
+    outgoing = "GET_OFFLINE_MESSAGES\t" + user
     clientSocket.send(outgoing.encode())
 
     incoming = clientSocket.recv(1024)
     print(incoming.decode('ascii'))
 
-def getNewMessages():
-    outgoing = "GET_NEW_MESSAGES \t JRUPP68"
+def getNewMessages(user):
+    outgoing = "GET_NEW_MESSAGES\t" + user
     clientSocket.send(outgoing.encode())
 
     incoming = clientSocket.recv(1024)
     print(incoming.decode('ascii'))
 
-def sendMessage():
-    print("Please enter username of the friend you want to message :")
-    user = input()
-    print("Enter the message :")
-    message = input()
-    print("Please enter your username :")
-    me = input()
-
-    outgoing = "SEND_MESSAGE \t " + me + "\t" + message + "\t" + user
+def sendMessage(sender, receiver, message):
+    outgoing = "SEND_MESSAGE \t " + sender + "\t" + message + "\t" + receiver
     clientSocket.send(outgoing.encode())
 
     incoming = clientSocket.recv(1024)
     print(incoming.decode('ascii'))
 
+def viewMessages(user, friend):
+    outgoing = "VIEW_ALL_MESSAGES\t" + user
+    clientSocket.send(outgoing.encode())
 
-action = input("Enter \n1 - show friends list \n2 - send message \n3 - register \n4 - login \n5- Add Friend \n:")
-print(action)
-if(action == "1"):
-    print("\n")
-    getFriends()
-elif(action == "2"):
-    print('\n')
-    sendMessage()
-elif(action == "3"):
-    print('\n')
-    register()
-elif(action == "4"):
-    print("\n")
-    login()
-elif(action == "5"):
-    print("\n")
-    addFriend()
+    incoming = clientSocket.recv(1024)
+    print(incoming.decode('ascii'))
 
+def logout(user):
+    outgoing = "LOGOUT\t" + user
+    clientSocket.send(outgoing.encode())
 
+while 1:
+    action = input("Enter \n1 - show friends list \n2 - send message \n3 - register \n4 - login \n5 - Add Friend \n6 - View Messages \n7 - Logout \n:")
+    print(action)
+    if(action == "1"):
+        print("\n")
+        user = input("Please enter your username :")
+        
+        getFriends(user)
+    elif(action == "2"):
+        print('\n')
+        print("Please enter username of the friend you want to message :")
+        receiver = input()
+        print("Enter the message :")
+        message = input()
+        print("Please enter your username :")
+        sender = input()
+        
+        sendMessage(sender, receiver, message)
+    elif(action == "3"):
+        print('\n')
+        username = input("Username :")
+        password = input("Password :")
+        passwordCheck = input("Password :")
+        firstName = input("First Name :")
+        lastName = input("Last Name :")
+        email = input("Email :")
+        
+        register(username, password, passwordCheck, firstName, lastName, email)
+    elif(action == "4"):
+        print("\n")
+        username = input("Username :")
+        password = input("Password :")
+        
+        login(username, password)
+    elif(action == "5"):
+        print("\n")
+        friend = input("Please enter the username of the friend you would like to add :")
+        user = input("Please enter your username :")
+        
+        addFriend(user, friend)
+    elif(action == "6"):
+        print("\n")
+        user = input("Please enter your username :")
+        friend = input("Please enter friend username of messsages you want to veiw :")
+        
+        viewMessages(user, friend)
+    elif(action == "7"):
+        print("\n")
+        user = input("Please enter your username :")
+
+        logout(user)
